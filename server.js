df8e68851504843195c1d136534000277a6b17b4
Muralidhar Kini <muralikini@users.noreply.github.com>
2023-12-12 09:44:42 +0530

    minor changes

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', (req, res) => {
   res.send('Hello from Node.js microservice!, WOW');
 });
 
+app.get('/hi',function(req,res){
+    res.send('{"response":"Hi Everyone"}');
+})
+
+app.get('/ready',function(req,res){
+    res.send('{"response":"Ready for the test ride!"}');
+})
+
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
 });
+module.exports=app;
