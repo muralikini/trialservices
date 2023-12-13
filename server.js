9e491038a5722f023cb1584e45ed484d1fa05cbd
Muralidhar Kini <muralikini@users.noreply.github.com>
2023-12-13 23:00:46 +0530

    wow

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js microservice!, WOW wow');
+  res.send('Hello from Node.js microservice!, WOW');
 });
 
 app.get('/hi',function(req,res){
