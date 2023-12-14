31ea6267ac2d05fef073cddf79beb8f25f6f1a4c
Muralidhar Kini <muralikini@users.noreply.github.com>
2023-12-14 10:33:00 +0530

    wow how do you feel now

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js microservice!, WOW How do you feel?');
+  res.send('Hello from Node.js microservice!, WOW How do you feel Now?');
 });
 
 app.get('/hi',function(req,res){
