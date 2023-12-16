018576803d28be659ebfa7c3e4a3f37b36c313fe
Muralidhar Kini <muralikini@users.noreply.github.com>
2023-12-16 21:07:29 +0530

    minor change

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js microservice! for testing?TEST6');
+  res.send('Hello from Node.js microservice! for testing?TEST7');
 });
 
 app.get('/hi',function(req,res){
